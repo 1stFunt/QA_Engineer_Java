ff5e7262e4bd96f69a521c2f28e0f0c2f423d77b
Funt <funt_88@mail.ru>
2024-08-05 14:24:03 +0300

    Initialization

---
+++ src/Lesson_2/Main.java
@@ -0,0 +1,7 @@
+package Lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Lesson_2/Tasks.java
@@ -0,0 +1,7 @@
+package Lesson_2;
+
+public class Tasks {
+    public static void main(String[] args) {
+
+    }
+}
