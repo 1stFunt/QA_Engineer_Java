df9855ba549b04c57d48df26a7f1232cf66d2ed3
Funt <funt_88@mail.ru>
2024-09-10 20:43:30 +0300

    Task with TestNG completed

--- src/Lesson_12_testng_5/FactorialCalculatorTest.java
+++ src/Lesson_12_testng_5/FactorialCalculatorTest.java
@@ -8,27 +8,27 @@ public class FactorialCalculatorTest {
 
     @Test
     public void testFactorialOfZero() {
-        Assert.assertEquals(FactorialCalculator.calculator(0), 1, "Факториал 0 должен быть 1");
+        Assert.assertEquals(calculator.calculateFactorial(0), 1, "Факториал 0 должен быть 1");
     }
 
     @Test
     public void testFactorialOfOne() {
-        Assert.assertEquals(FactorialCalculator.calculator(1), 1, "Факториал 1 должен быть 1");
+        Assert.assertEquals(calculator.calculateFactorial(1), 1, "Факториал 1 должен быть 1");
     }
 
     @Test
     public void testFactorialOfPositiveNumber() {
-        Assert.assertEquals(FactorialCalculator.calculator(5), 120, "Факториал 5 должен быть 120");
+        Assert.assertEquals(calculator.calculateFactorial(5), 120, "Факториал 5 должен быть 120");
     }
 
     @Test
     public void testFactorialOfLargeNumber() {
-        Assert.assertEquals(FactorialCalculator.calculator(20), 2432902008176640000L, "Факториал 20 должен быть 2432902008176640000");
+        Assert.assertEquals(calculator.calculateFactorial(20), 2432902008176640000L, "Факториал 20 должен быть 2432902008176640000");
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testFactorialOfNegativeNumber() {
-        FactorialCalculator.calculator(-1);
+        calculator.calculateFactorial(-1);
         Assert.fail("Должно выбрасываться исключение для отрицательных чисел");
     }
 }
