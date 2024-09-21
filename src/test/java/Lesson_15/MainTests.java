873f3718a47195ce785038e040ccae0245da794f
Funt <funt_88@mail.ru>
2024-09-21 03:22:59 +0300

    Homework 15 finished

--- src/test/java/Lesson_15/MainTests.java
+++ src/test/java/Lesson_15/MainTests.java
@@ -57,18 +57,20 @@ public class MainTests {
                 currentUrl, "URL не соответствует ожидаемому.");
     }
 
-    // Заполнить поля и проверить работу кнопки "Продолжить"
+    // 4. Заполнить поля и проверить работу кнопки "Продолжить"
     @Test
     public void testInputFieldsAndSubmit() throws InterruptedException {
         mainPage.enterPhoneNumber("297777777");
-        mainPage.enterSum("100");
+        String expectedSum = "100.00";
+        mainPage.enterSum(expectedSum);
         mainPage.enterEmail("funt_88@mail.ru");
         mainPage.clickContinueButton();
         Thread.sleep(3000); // Ожидание
         // Ожидание загрузки и переключение на фрейм
         driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.bepaid-iframe")));
-        assertEquals("100.00 BYN", mainPage.getCardPageText(),
-                "Текст на странице карты не соответствует ожидаемому.");
+        assertTrue(mainPage.getCardPageText().contains(expectedSum + " BYN"),
+                "Текст на странице карты не содержит сумму и валюту.");
+        ;
 
     }
 
