cc32daec64e4708e6bb791e231e7ca3afc6f32bb
Funt <funt_88@mail.ru>
2024-09-29 23:57:08 +0300

    Homework 17 in processed

--- src/test/java/Lesson_17/ApiTest.java
+++ src/test/java/Lesson_17/ApiTest.java
@@ -17,5 +17,21 @@ public class ApiTest {
                 .and().body("args.foo1", equalTo("bar1")) // Проверка аргумента foo1
                 .body("args.foo2", equalTo("bar2")); // Проверка аргумента foo2
     }
+
+    @Test
+    public void testPostRequest() {
+        // Выполняем POST-запрос
+        given()
+                .baseUri("https://postman-echo.com") // Установка базового URL
+                .contentType("text/plain") // Установка типа контента
+                .body("This is expected to be sent back as part of response body.") // Тело запроса
+                .when()
+                .post("/post") // Выполняем POST-запрос
+                .then()
+                .log().body() // Логирование тела ответа
+                .statusCode(200) // Проверка статуса кода
+                .and()
+                .body("data", equalTo("This is expected to be sent back as part of response body.")); // Проверка данных в ответе
+    }
 }
 // В процессе, закончу через часик. Спасибо! =)
